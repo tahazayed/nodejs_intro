9c14eee060c2f6593438ecf9bb85ae00e5e2436f
Taha Elsayed <tahazayed@gmail.com>
2023-05-15 15:53:58 -0500

    Template Literal Version of Simple Adder

--- N4/AdderWebServer.js
+++ N4/AdderWebServer.js
@@ -2,17 +2,20 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var addmod = require('./addmod.js');
+var simaddmod = require('./simaddmod.js');
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
     if (q.pathname == "/add.js")
-        addmod.add(req, res, q.query)
+        addmod.add(req, res, q.query);
+    else if (q.pathname == "/SimpleAdder.html")
+        simaddmod.add(req, res, q.query)
     else
         fs.readFile(filename, function (err, data) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             if (err) {
-               
+
                 return res.end("404 Not Found");
             }
             res.writeHead(200); // Content Type not included
